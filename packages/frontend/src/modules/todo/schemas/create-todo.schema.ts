61edfc99fb1e7c0db6eea55e00049679d4dbd553
sizuchok <sizuchok0@gmail.com>
2023-11-21 19:15:19 +0200

    feat: Add create todo and update todo joi validation schemas

---
+++ packages/frontend/src/modules/todo/schemas/create-todo.schema.ts
@@ -0,0 +1,10 @@
+import * as Yup from 'yup';
+
+export const CreateTodoSchema = Yup.object().shape({
+  title: Yup.string()
+    .min(3, 'Title must be at least 3 characters long')
+    .max(50, 'Title must be at most 16 characters long')
+    .required("Don't forget to name your task"),
+  description: Yup.string().optional(),
+  isPublic: Yup.boolean().required()
+});
