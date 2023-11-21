36b7feaaadd2a0085f3a3ce7b4a324295d053df6
sizuchok <sizuchok0@gmail.com>
2023-11-21 19:22:35 +0200

    feat: Add TodoCard component

---
+++ packages/frontend/src/modules/todo/components/todo-card/todo-card.styled.tsx
@@ -0,0 +1,25 @@
+import { styled } from 'styled-components';
+import { COLORS, FONTS } from '../../../theme';
+import { rem } from '../../../theme/rem.util';
+
+export const TodoCard = styled.div`
+  background-color: ${COLORS.white};
+  font-family: ${FONTS.FAMILIES.primary};
+  min-height: ${rem(300)};
+  padding: ${rem(20)};
+  display: flex;
+  flex-direction: column;
+`;
+
+export const TodoCardTitle = styled.h4`
+  font-size: ${FONTS.SIZES.l};
+  margin-bottom: ${rem(10)};
+`;
+
+export const TodoCardDescription = styled.p`
+  font-size: ${FONTS.SIZES.m};
+`;
+
+export const TodoCardActions = styled.div`
+  margin: auto auto 0 auto;
+`;
