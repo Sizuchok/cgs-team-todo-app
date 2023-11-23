b745927d64885d77b1a17f01866c4e3359fc1e76
sizuchok <sizuchok0@gmail.com>
2023-11-23 16:04:01 +0200

    bug: Check ownership middleware now takes in count if todo is public or not

--- packages/backend/src/middleware/checkOwnership.ts
+++ packages/backend/src/middleware/checkOwnership.ts
@@ -1,25 +1,22 @@
 import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { UserDto } from '../dto/user/user.dto';
-import { Todo } from '../entities/todo.entity';
 import { HttpError } from '../exceptions/HttpError';
+import { todoService } from '../services/todo.service';
 
 export const checkOwnership = async (req: Request, _: Response, next: NextFunction) => {
   const todoId = req.params.id;
   const userId = (req.user as UserDto)?.id;
 
   try {
-    const todo = await Todo.findOneBy({
-      id: todoId,
-      userId
-    });
+    const todo = await todoService.findTodoById(todoId);
 
-    if (!todo) {
-      throw new HttpError(StatusCodes.FORBIDDEN, 'You do not have permission to edit this todo');
-    }
+    if (todo.isPublic) return next();
+
+    if (todo.userId === userId) return next();
+
+    throw new HttpError(StatusCodes.FORBIDDEN, 'You do not have permission to edit this todo');
   } catch (error) {
     next(error);
   }
-
-  next();
 };
