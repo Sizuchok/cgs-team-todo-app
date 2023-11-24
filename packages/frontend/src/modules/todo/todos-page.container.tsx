c3c04d383fb81be47b622526d25a4b2a8eaacbf8
sizuchok <sizuchok0@gmail.com>
2023-11-25 01:51:39 +0200

    feat: Add filters to functionality to todos page

--- packages/frontend/src/modules/todo/todos-page.container.tsx
+++ packages/frontend/src/modules/todo/todos-page.container.tsx
@@ -1,10 +1,14 @@
-import { useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
+import { useDebounce } from 'use-debounce';
 import Button from '../common/components/button/button.component';
 import Filters from '../common/components/filters/todo-filters.component';
 import PrimaryLayout from '../common/components/layouts/primary-layout/primary-layout.component';
+import LoadingOverlay from '../common/components/loading-overlay/loading-overlay.component';
 import Modal from '../common/components/modal/modal-container/modal.component';
 import SearchBar from '../common/components/search-bar/search-bar.component';
+import ToggleButton from '../common/components/toggle-button/toggle-button.component';
 import useMedia from '../common/hooks/use-media.hook';
+import { GetAllTodosFilters } from '../common/types/todo.types';
 import CreateTodoForm from './components/forms/create-todo-form.component';
 import TodoModalLayout from './components/todo-modal-layout/todo-modal-layout.component';
 import TodosDesktop from './components/todos-desktop/todos-desktop.component';
@@ -14,26 +18,79 @@ import { useGetAllTodos } from './hooks/get-all-todos.hook';
 import * as StyledCommon from './todos-page.styled';
 
 const TodosPage = () => {
+  const [query, setQuery] = useState<string>('');
+  const [debouncedQuery] = useDebounce(query, 500);
+
   const [openCreateTodoModal, setOpenCreateTodoModal] = useState<boolean>(false);
+
+  const [queryParams, setQueryParams] = useState<GetAllTodosFilters>({});
+
+  const { data: todos, isFetching } = useGetAllTodos(queryParams);
+
+  const handleQueryChange = ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
+    setQuery(value);
+  };
+
+  const handleSetPrivate = () => {
+    setQueryParams({
+      ...queryParams,
+      isPrivate: !queryParams.isPrivate
+    });
+  };
+
+  const handleSetPublic = () => {
+    setQueryParams({
+      ...queryParams,
+      isPublic: !queryParams.isPublic
+    });
+  };
+
+  const handleSetChecked = () => {
+    setQueryParams({
+      ...queryParams,
+      isChecked: !queryParams.isChecked
+    });
+  };
+
+  const handleSetAll = () => {
+    setQueryParams({});
+  };
 
   const handleOpenCreateModal = () => setOpenCreateTodoModal(true);
   const handleCloseCreateModal = () => setOpenCreateTodoModal(false);
 
   const { isDesktop, isMobile, isTablet } = useMedia();
 
-  const { data: todos } = useGetAllTodos();
+  useEffect(() => {
+    setQueryParams({
+      ...queryParams,
+      query: debouncedQuery === '' ? undefined : debouncedQuery
+    });
+  }, [debouncedQuery]);
 
   return (
     <PrimaryLayout>
       <StyledCommon.FiltersContainer>
-        <Filters />
+        <Filters>
+          <ToggleButton title="All" onClick={handleSetAll} />
+          <ToggleButton title="Public" onClick={handleSetPublic} />
+          <ToggleButton title="Private" onClick={handleSetPrivate} />
+          <ToggleButton title="Completed" onClick={handleSetChecked} />
+        </Filters>
+
         <Button secondary onClick={handleOpenCreateModal} type="button" title="New todo" />
-        <SearchBar query="" onChange={() => {}} />
+        <SearchBar query={query} onChange={handleQueryChange} />
       </StyledCommon.FiltersContainer>
 
-      {isDesktop && <TodosDesktop todos={todos} />}
-      {isTablet && <TodosTablet todos={todos} />}
-      {isMobile && <TodosMobile todos={todos} />}
+      {isFetching ? (
+        <LoadingOverlay />
+      ) : (
+        <>
+          {isDesktop && <TodosDesktop todos={todos} />}
+          {isTablet && <TodosTablet todos={todos} />}
+          {isMobile && <TodosMobile todos={todos} />}
+        </>
+      )}
 
       <Modal open={openCreateTodoModal} onClose={handleCloseCreateModal}>
         <TodoModalLayout>
--- packages/frontend/src/modules/todo/todos-page.styled.tsx
+++ packages/frontend/src/modules/todo/todos-page.styled.tsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
+import { rem } from '../theme/rem.util';
 
 export const FiltersContainer = styled.div`
   display: flex;
   justify-content: space-between;
+  flex-wrap: wrap;
+  gap: ${rem(20)};
 `;
