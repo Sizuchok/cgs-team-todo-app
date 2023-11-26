021fa34201913b0da7aaf17bc0e8bb9e0f3bc232
sizuchok <sizuchok0@gmail.com>
2023-11-26 22:03:10 +0200

    bug: Get all todos schema now allows zeros

--- packages/backend/src/schemas/todo/get-all-todos.schema.ts
+++ packages/backend/src/schemas/todo/get-all-todos.schema.ts
@@ -2,8 +2,8 @@ import Joi from 'joi';
 import { GetAllTodosFiltersDto } from '../../dto/todo/get-all-todos.dto';
 
 export const getAllTodosSchema = Joi.object<GetAllTodosFiltersDto>({
-  limit: Joi.number().positive().integer().optional(),
-  offset: Joi.number().positive().integer().optional(),
+  limit: Joi.number().positive().allow(0).integer().optional(),
+  offset: Joi.number().positive().allow(0).integer().optional(),
   isPublic: Joi.boolean().optional(),
   isChecked: Joi.boolean().optional(),
   isPrivate: Joi.boolean().optional(),
